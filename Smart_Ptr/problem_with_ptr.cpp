60935a940fd5516ed699e6a845346f1677029464
Sneha Bickunde <111409354+snehabichkunde@users.noreply.github.com>
2023-06-16 14:20:03 +0530

    Create problem_with_ptr.cpp

---
+++ Smart_Ptr/problem_with_ptr.cpp
@@ -0,0 +1,18 @@
+// the problem with pointer
+/*
+#include<iostream>
+using namespace std;
+void fun ()
+{
+    int * ptr = new int[10];
+}
+int main()
+{
+    while (True){
+        fun();
+    }
+    return 0;
+}
+*/
+
+// this program will crash and compiler won't give warning about it already
